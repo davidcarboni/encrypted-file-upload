caab8236efee4114c9023c9324094ff88e93fbae
David Carboni <david@carboni.io>
2018-05-16 11:14:55 +0100

    Added a bunch of unimplemented methods.

--- src/test/java/org/apache/commons/fileupload/MockHttpServletRequest.java
+++ src/test/java/org/apache/commons/fileupload/MockHttpServletRequest.java
@@ -22,15 +22,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class MockHttpServletRequest implements HttpServletRequest {
 
@@ -218,6 +216,11 @@ public class MockHttpServletRequest implements HttpServletRequest {
         return null;
     }
 
+    @Override
+    public String changeSessionId() {
+        return null;
+    }
+
     /**
      * @see javax.servlet.http.HttpServletRequest#isRequestedSessionIdValid()
      */
@@ -246,6 +249,36 @@ public class MockHttpServletRequest implements HttpServletRequest {
     @Deprecated
     public boolean isRequestedSessionIdFromUrl() {
         return false;
+    }
+
+    @Override
+    public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+        return false;
+    }
+
+    @Override
+    public void login(String username, String password) throws ServletException {
+
+    }
+
+    @Override
+    public void logout() throws ServletException {
+
+    }
+
+    @Override
+    public Collection<Part> getParts() throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public Part getPart(String name) throws IOException, ServletException {
+        return null;
+    }
+
+    @Override
+    public <T extends HttpUpgradeHandler> T upgrade(Class<T> handlerClass) throws IOException, ServletException {
+        return null;
     }
 
     /**
@@ -291,6 +324,11 @@ public class MockHttpServletRequest implements HttpServletRequest {
             iLength = (int) length;
         }
         return iLength;
+    }
+
+    @Override
+    public long getContentLengthLong() {
+        return 0;
     }
 
     /**
@@ -394,6 +432,41 @@ public class MockHttpServletRequest implements HttpServletRequest {
     @SuppressWarnings("javadoc") // This is a Servlet 2.4 method
     public int getLocalPort() {
         return 0;
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync() throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+        return null;
+    }
+
+    @Override
+    public boolean isAsyncStarted() {
+        return false;
+    }
+
+    @Override
+    public boolean isAsyncSupported() {
+        return false;
+    }
+
+    @Override
+    public AsyncContext getAsyncContext() {
+        return null;
+    }
+
+    @Override
+    public DispatcherType getDispatcherType() {
+        return null;
     }
 
     /**
@@ -510,6 +583,22 @@ public class MockHttpServletRequest implements HttpServletRequest {
             return in.read(b, off, len);
         }
 
+        @Override
+        public boolean isFinished() {
+            // Default implementation
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            // Default implementation
+            return false;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+            // Default implementation
+        }
     }
 
 }
